27f0a0adecba418db750f39325bec2ea449992b2
Michael F. Schönitzer <michael@schoenitzer.de>
2020-08-07 18:04:32 +0200

    Add filters

--- populate.js
+++ populate.js
@@ -32,11 +32,12 @@ var main = (function () {
   var load = function () {
     return new Promise(function (resolve, reject) {
       var xhttp = new XMLHttpRequest()
-      xhttp.open('GET', './mpv_script_directory.json', true)
+      xhttp.open('GET', 'https://raw.githubusercontent.com/Nudin/mpv-script-directory/master/mpv_script_directory.json', true)
       xhttp.onload = function () {
         if (xhttp.status === 200) {
           var rows = JSON.parse(xhttp.response)
           addrows(rows)
+          updatefilters()
           resolve()
         } else {
           reject(Error(xhttp.response))
@@ -49,11 +50,22 @@ var main = (function () {
   var addrows = function(rows) {
     var tbody = document.getElementById("tbody")
     var t = document.querySelector('#scriptrow'),
+    tr = t.content.querySelector("tr");
     link = t.content.querySelector(".name");
     stars = t.content.querySelector(".stars");
     desc = t.content.querySelector(".desc");
     for (var id in rows) {
       row = rows[id]
+      if ( !("os" in row) || row.os.length == 0) {
+        tr.classList = ["Portable"]
+      }
+      else {
+        tr.className = ""
+        tr.classList.add(...row.os)
+      }
+      var type = row.type.split(' ')[0]
+      if ( type === "user" ) { type = "shader" ; }
+      tr.classList.add(type)
       link.href = row.url
       link.innerText = row.name
       stars.innerText = row.stars || ""
@@ -66,17 +78,48 @@ var main = (function () {
     }
   }
 
+  var updatefilters = function (filter) {
+    zebracount = 0
+    document.querySelectorAll("tbody tr").forEach(tr => {
+      var cl = Array.from(tr.classList)
+      tr.style.display = "none"
+      tr.style.backgroundColor = null
+      var hide = true
+      document.querySelectorAll('.osfilter:checked').forEach(function(filter) {
+        if (cl.includes(filter.id)) {
+          hide = false
+        }
+      } )
+      if (hide) {
+        return
+      }
+      var hide = true
+      document.querySelectorAll('.typefilter:checked').forEach(function(filter) {
+        if (cl.includes(filter.id)) {
+          hide = false
+        }
+      } )
+      if (!hide) {
+        tr.style.display = "";
+        if (zebracount % 2 == 0) {
+          tr.style.backgroundColor = "#eee"
+        }
+        zebracount++;
+      }
+    })
+  }
+
   var init = function () {
     load()
     // sortable table, based on https://stackoverflow.com/a/19947532
     document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
-      console.log(th);
       const table = th.closest('table').querySelector("tbody");
       Array.from(table.querySelectorAll('tr:nth-child(n+1)'))
         .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
         .forEach(tr => table.appendChild(tr) );
     })));
 
+    document.querySelectorAll('.filter').forEach(filter => filter.onclick = updatefilters)
   }
 
   return {

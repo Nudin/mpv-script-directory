2fcbe14469a9aee65a19b33b8e29fae8b3df03d1
Michael F. Schönitzer <michael@schoenitzer.de>
2020-08-22 16:35:58 +0200

    Clean up and fix bugs

--- populate.js
+++ populate.js
@@ -1,43 +1,21 @@
-
-function Search() {
-  // Declare variables
-  var input, filter, table, tr, td, i, txtValue;
-  input = document.getElementById("search");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("main");
-  tr = table.getElementsByTagName("tr");
-
-  // Loop through all table rows, and hide those who don't match the search query
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    }
-  }
-}
-
 var main = (function () {
+  const getCellValue = (tr, idx) => tr.children[idx].dataset['sortvalue'] || tr.children[idx].innerText || tr.children[idx].textContent
 
-  const getCellValue = (tr, idx) => tr.children[idx].dataset["sortvalue"] || tr.children[idx].innerText || tr.children[idx].textContent;
-
-  const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
+  const comparer = (idx, asc) => (a, b) => ((v1, v2) =>
     v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v2 - v1 : v1.toString().localeCompare(v2)
-  )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+  )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx))
 
+  // load and parse the mpv script directory and update the table
   var load = function () {
     return new Promise(function (resolve, reject) {
-      var xhttp = new XMLHttpRequest()
+      var xhttp = new XMLHttpRequest() // eslint-disable-line no-undef
       xhttp.open('GET', 'https://raw.githubusercontent.com/Nudin/mpv-script-directory/master/mpv_script_directory.json', true)
       xhttp.onload = function () {
         if (xhttp.status === 200) {
           var rows = JSON.parse(xhttp.response)
           addrows(rows)
           updatefilters()
+          search()
           resolve()
         } else {
           reject(Error(xhttp.response))
@@ -47,86 +25,130 @@ var main = (function () {
     })
   }
 
-  var addrows = function(rows) {
-    var tbody = document.getElementById("tbody")
-    var t = document.querySelector('#scriptrow'),
-    tr = t.content.querySelector("tr");
-    link = t.content.querySelector(".name");
-    scriptid = t.content.querySelector(".scriptid");
-    stars = t.content.querySelector(".stars");
-    desc = t.content.querySelector(".desc");
-    for (var id in rows) {
-      row = rows[id]
-      if ( !("os" in row) || row.os.length == 0) {
-        tr.classList = ["Portable"]
-      }
-      else {
-        tr.className = ""
-        tr.classList.add(...row.os)
-      }
-      var type = row.type.split(' ')[0]
-      if ( type === "user" ) { type = "shader" ; }
-      tr.classList.add(type)
-      scriptid.innerText = id
-      scriptid.title = "ScriptID"
-      link.href = row.url
-      link.innerText = row.name
-      stars.innerText = row.stars || ""
-      if (row.sharedrepo && row.stars) {
-        stars.innerText += '*'
-      }
-      stars.dataset["sortvalue"] = row.stars || 0
-      desc.innerText = row.desc
-      tbody.appendChild(document.importNode(t.content, true))
-    }
+  var shortenType = {
+    'javascript': 'javascript', // eslint-disable-line quote-props
+    'lua script': 'lua',
+    'user shader': 'shader',
+    'C plugin': 'C',
+    'other': 'other', // eslint-disable-line quote-props
+    'vapoursynth script': 'other'
   }
 
+  // Add a table row for each script
+  var addrows = function (scripts) {
+    var tbody = document.getElementById('tbody')
+    var t = document.querySelector('#scriptrow')
+    var tr = t.content.querySelector('tr')
+    var link = t.content.querySelector('.name')
+    var scriptid = t.content.querySelector('.scriptid')
+    var stars = t.content.querySelector('.stars')
+    var desc = t.content.querySelector('.desc')
+    for (var id in scripts) {
+      var script = scripts[id]
+      scriptid.innerText = id
+      link.innerText = script.name
+      desc.innerText = script.desc
+      link.href = script.url
+      stars.innerText = script.stars || ''
+      if (!('os' in script) || script.os.length === 0) {
+        tr.classList = ['Portable']
+      } else {
+        tr.className = ''
+        tr.classList.add(...script.os)
+      }
+      tr.classList.add(shortenType[script.type])
+      if (script.sharedrepo && script.stars) {
+        stars.innerText += '*'
+      }
+      stars.dataset['sortvalue'] = script.stars || 0
+      tbody.appendChild(document.importNode(t.content, true))
+    }
+    updatefilters()
+  }
+
+  // Limit the shown rows to those matching the search term
+  var search = function () {
+    var input, filter, table, tr, td, i, txtValue
+    input = document.getElementById('search')
+    filter = input.value.toUpperCase()
+    table = document.getElementById('main')
+    tr = table.getElementsByTagName('tr')
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName('td')[0]
+      if (td) {
+        txtValue = td.textContent || td.innerText
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].classList.remove('hidden-by-search')
+        } else {
+          tr[i].classList.add('hidden-by-search')
+        }
+      }
+    }
+    updateZebra()
+  }
+
+  // Limit the shown rows to those matching the filters
   var updatefilters = function (filter) {
-    zebracount = 0
-    document.querySelectorAll("tbody tr").forEach(tr => {
+    document.querySelectorAll('tbody tr').forEach(tr => {
       var cl = Array.from(tr.classList)
-      tr.style.display = "none"
-      tr.style.backgroundColor = null
-      var hide = true
-      document.querySelectorAll('.osfilter:checked').forEach(function(filter) {
+
+      tr.classList.add('hidden-by-os-filter')
+      document.querySelectorAll('.osfilter:checked').forEach(function (filter) {
         if (cl.includes(filter.id)) {
-          hide = false
+          tr.classList.remove('hidden-by-os-filter')
         }
-      } )
-      if (hide) {
-        return
-      }
-      var hide = true
-      document.querySelectorAll('.typefilter:checked').forEach(function(filter) {
+      })
+
+      tr.classList.add('hidden-by-type-filter')
+      document.querySelectorAll('.typefilter:checked').forEach(function (filter) {
         if (cl.includes(filter.id)) {
-          hide = false
+          tr.classList.remove('hidden-by-type-filter')
         }
-      } )
-      if (!hide) {
-        tr.style.display = "";
-        if (zebracount % 2 == 0) {
-          tr.style.backgroundColor = "#eee"
-        }
-        zebracount++;
-      }
+      })
     })
+    updateZebra()
+  }
+
+  // Update zebra
+  var updateZebra = function () {
+    var zebracount = 0
+    document.querySelectorAll(
+      'tbody tr' +
+      ':not(.hidden-by-os-filter)' +
+      ':not(.hidden-by-type-filter)' +
+      ':not(.hidden-by-search)').forEach(tr => {
+      if (zebracount % 2 === 0) {
+        tr.style.backgroundColor = '#eee'
+      } else {
+        tr.style.backgroundColor = ''
+      }
+      zebracount++
+    }
+    )
+  }
+
+  // Sort table by selected column
+  // based on https://stackoverflow.com/a/19947532
+  var sortRows = function () {
+    var th = this
+    const table = th.closest('table').querySelector('tbody')
+    Array.from(table.querySelectorAll('tr:nth-child(n+1)'))
+      .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+      .forEach(tr => table.appendChild(tr))
+    updateZebra()
   }
 
   var init = function () {
     load()
-    // sortable table, based on https://stackoverflow.com/a/19947532
-    document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
-      const table = th.closest('table').querySelector("tbody");
-      Array.from(table.querySelectorAll('tr:nth-child(n+1)'))
-        .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
-        .forEach(tr => table.appendChild(tr) );
-    })));
-
-    document.querySelectorAll('.filter').forEach(filter => filter.onclick = updatefilters)
+    document.querySelector('#search').onkeyup = search
+    document.querySelectorAll('th').forEach(th => th.addEventListener('click', sortRows))
+    document.querySelectorAll('.filter').forEach(filter => { filter.onclick = updatefilters })
   }
 
   return {
-    init: init,
+    init: init
   }
 })()
 
